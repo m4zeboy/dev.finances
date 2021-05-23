27923236b2391f6176fbc4288050be512796ab51
unknown <moises775azevedo@gmail.com>
2021-05-23 14:45:39 -0300

    Segunda parte da aula 3

--- scripts.js
+++ scripts.js
@@ -56,17 +56,36 @@ const Transaction = {
     }
 }
 
+const Utils = {
+    formatCurrency(value){
+        const signal = Number(value) < 0? "-": "";
+    }
+}
+
 const DOM = {
-    innerHTMLTransaction() {
+    transactionsContainer: document.querySelector('table#data-table tbody'),
+    addTransaction(transaction, index){
+        const tr = document.createElement('tr');
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction);
+        DOM.transactionsContainer.appendChild(tr)
+    },
+    innerHTMLTransaction(transaction) {
+        const CSSclass = transaction.amount > 0 ? 'income' : 'expense';
+
+        const amount = Utils.formatCurrency(transaction.amount)
+
         const html = `
-        <tr>
-            <td class="description">Energia Elétrica</td>
-            <td class="expense">- R$ 500,00</td>
-            <td class="date">09/02/2021</td>
+            <td class="description">${transaction.description}</td>
+            <td class="${CSSclass}">${transaction.amount}</td>
+            <td class="date">${transaction.date}</td>
             <td>
                 <img src="./assets/minus.svg" alt="Remover Transação">
             </td>
-        </tr>
-    `
+            `;
+        return html;
     }
 }
+
+transactions.forEach(function(transaction) {
+    DOM.addTransaction(transaction)
+})
