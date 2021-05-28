d18a6a1e4d3292df874ca8bfc108216996f6f384
unknown <moises775azevedo@gmail.com>
2021-05-28 19:55:13 -0300

    Corrigindo erro na formatação do amount

--- scripts.js
+++ scripts.js
@@ -84,8 +84,8 @@ const Utils = {
         return signal + value;
     },
     formatAmount(value) {
-        value = Number(value.replace(/\,\./g, "")) * 100
-        return value
+        value = Number(value) * 100
+        return Math.round(value)
     },
     formatDate(date) {
         const splittedDate = date.split("-")
